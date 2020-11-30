b873e7bf7e4e2dc62692b7cc032b3393ec94f2b4
Nixon Rios Sanchez <nixonrs@hotmail.com>
2020-11-29 22:39:17 -0500

    tarea 2 con responsive, se agrega video

---
+++ pagina2.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina 2</title>
+    <link rel="stylesheet" type="text/css" href="css/estilos2.css">
+</head>
+<body>
+    <div class="container">
+            <header>
+                    <h1>Pagina 2</h1>
+                    <h2>Web Fans Juego de Tronos</h2>
+            </header>
+                <nav>
+                    <ul>
+                        <li id="menu1"><a href="index.php">Volver</a></li>
+                        <li id="menu2">Temporada 1</li>
+                        <li id="menu3">Temporada 2</li>
+                        <li id="menu4">Temporada 3</li>
+                    </ul>
+                    <ul>    
+                        <li id="menu5">Temporada 4</li>
+                        <li id="menu6">Temporada 5</li>
+                        <li id="menu7">Temporada 6</li>
+                        <li id="menu8">Temporada 7</li>
+                        <li id="menu8">Temporada 8</li>       
+                    </ul>
+                </nav>
+                <section>
+                    <p>
+                        Game of Thrones —llamada Juego de Tronos en España—
+                    </p> 
+                    <p>
+                    es una serie de televisión de drama y fantasía medieval creada por David Benioff y D. B. Weiss y producida por la cadena HBO. Su argumento está inspirado en la serie de novelas Canción de hielo y fuego, escrita por el estadounidense George R. R. Martin, y relata las vivencias de un grupo de personajes de distintas casas nobiliarias en el continente ficticio de Poniente para tener el control del Trono de Hierro y gobernar los siete reinos que conforman el territorio.1​
+                    El desarrollo de la serie se remonta a 2006, cuando Benioff y Weiss tuvieron una reunión con Martin para discutir ideas en torno a una adaptación televisiva de su material literario.2​3​ Si bien los guiones del episodio piloto, «Winter is Coming», quedaron concluidos en 2008,4​5​ las grabaciones tuvieron lugar en 2010 bajo la dirección de Tim Van Patten.6​ A lo largo de ocho temporadas y más de sesenta episodios, Game of Thrones cubre el argumento de las primeras cinco novelas de Martin con ciertas modificaciones7​ y la adición de material inédito a partir de una descripción del autor sobre los eventos futuros de su universo literario.8​ Se trata de una de las series más costosas en la historia de la televisión,9​ cuyo rodaje llegó a involucrar hasta cuatro unidades de filmación en paralelo10​ y la producción de efectos visuales requirió de la participación simultánea de hasta catorce estudios en distintos países.11​
+                    </p>
+                </section>
+                <aside>
+                    Aside
+                    <br>
+                    <img class="tamano" src="images/images1.jpg">
+                </aside>
+                <footer>
+                    <p>
+                        Este es el pie de pagina
+                    </p>
+                </footer>   
+    </div>
+    
+</body>
+</html>
